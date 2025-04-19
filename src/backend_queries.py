057f10dfa2416c63732f6fc94058b562be71e9ba
AgiGames <155605209+AgiGames@users.noreply.github.com>
2025-04-19 16:50:22 +0530

    Gave Functionalities to Doctor Dashboard

--- src/backend_queries.py
+++ src/backend_queries.py
@@ -60,3 +60,88 @@ def get_all_doctors() -> List[Tuple]:
     results = cursor.fetchall()
     cursor.close()
     return results       
+
+def get_doctor_id(user_id: int) -> int:
+
+    global conn
+    if conn is None:
+        init_connection()
+
+    cursor = conn.cursor()
+    query = "SELECT DoctorID FROM doctors WHERE UserID = %s"
+    cursor.execute(query, (user_id,))
+    results = cursor.fetchone()
+    cursor.close()
+    return results[0]
+
+def get_appointments(doctor_id: int) -> List[Tuple]:
+
+    global conn
+    if conn is None:
+        init_connection()
+
+    '''
+    SELECT * FROM (
+    SELECT a.AppointmentID, p.PatientID, p.Name AS "Patient Name", a.DoctorID, d.Name AS "Doctor Name", a.DateTime, a.Status
+    FROM patients p
+    JOIN appointment a ON p.PatientID = a.PatientID
+    JOIN doctors d ON d.DoctorID = a.DoctorID
+    ) AS result
+    WHERE DoctorID = 2;
+    '''
+
+    cursor = conn.cursor()
+    query = "SELECT * FROM (SELECT a.AppointmentID, p.PatientID, p.Name AS \"Patient Name\", a.DoctorID, d.Name AS \"Doctor Name\", a.DateTime, a.Status FROM patients p JOIN appointment a ON p.PatientID = a.PatientID JOIN doctors d ON d.DoctorID = a.DoctorID) AS result WHERE DoctorID = %s;"
+    cursor.execute(query, (doctor_id,))
+    result = cursor.fetchall()
+    cursor.close()
+    return result
+
+def get_doctor_availability(user_id: int) -> str:
+
+    global conn
+    if conn is None:
+        init_connection()
+
+    cursor = conn.cursor()
+    query = "SELECT Availability FROM doctors WHERE UserID = %s"
+    cursor.execute(query, (user_id,))
+    result = cursor.fetchone()[0]
+    cursor.close()
+    return result
+
+def set_doctor_availability(user_id: int, available_state: str) -> None:
+
+    global conn
+    if conn is None:
+        init_connection()
+
+    cursor = conn.cursor()
+    query = "UPDATE doctors SET Availability = %s WHERE UserID = %s"
+
+    try:
+        cursor.execute(query, (available_state, user_id))
+        conn.commit()
+    except mysql.connector.Error as err:
+        print("Error setting availability", err)
+        rows_affected = 0
+    finally:
+        cursor.close()
+
+def set_appointment_as_completed(appointment_id: int) -> None:
+
+    global conn
+    if conn is None:
+        init_connection()
+
+    cursor = conn.cursor()
+    query = "UPDATE appointment SET Status = \"Completed\" WHERE AppointmentID = %s"
+
+    try:
+        cursor.execute(query, (appointment_id,))
+        conn.commit()
+    except mysql.connector.Error as err:
+        print("Error setting appointment completion status", err)
+        rows_affected = 0
+    finally:
+        cursor.close()
